238e31a09e9580e35dab8ae22357b17e760fc0c0
Lacoruna <tjfrem@hanyang.ac.kr>
2024-01-03 16:46:03 +0900

    GameManager 완성
    GameManager script와 Loader script 작성

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance = null;
+    private BoardManager boardScript;
+    private int level = 3;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
         
+        DontDestroyOnLoad(gameObject);
+        
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
     }
 
     // Update is called once per frame
